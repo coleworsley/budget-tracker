c78febc788856ccc19c6bfd4f3be7da34c241541
coleworsley <cole.s.worsley@gmail.com>
2018-02-19 18:13:08 -0700

    add dependencies and update db schema

--- db/migrations/20180209175942_initial.js
+++ db/migrations/20180209175942_initial.js
@@ -1,8 +1,13 @@
+exports.up = (knex, Promise) =>
+  Promise.all([
+    knex.schema.createTable('user', table => {
+      table.increments('id').primary();
+      table.string('email');
+      table.string('password');
+      table.string('first_name');
+      table.string('last_name');
+      table.timestamps(true, true);
+    })
+  ]);
 
-exports.up = function(knex, Promise) {
-  
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = (knex, Promise) => Promise.all([knex.schema.dropTable('user')]);
